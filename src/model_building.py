63e2be9fa136483b0fe394e777f2355245320742
souvikpaul1 <souvikpaul425@gmail.com>
2025-05-08 19:28:10 +0530

    executed model building and model_eval

--- src/model_building.py
+++ src/model_building.py
@@ -119,7 +119,11 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = load_params('params.yaml')['model_building']
+        #params = load_params('params.yaml')['model_building']
+        params = {
+            'n_estimators': 100,
+            'random_state': 42
+        }
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
