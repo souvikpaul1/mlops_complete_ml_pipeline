03fb31c91713ec214d848a436912861703e2eff7
souvikpaul1 <souvikpaul425@gmail.com>
2025-05-08 08:00:24 +0530

    code executed till feature_engineering

--- src/model_building.py
+++ src/model_building.py
@@ -27,7 +27,7 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
-
+'''
 def load_params(params_path: str) -> dict:
     """Load parameters from a YAML file."""
     try:
@@ -44,7 +44,7 @@ def load_params(params_path: str) -> dict:
     except Exception as e:
         logger.error('Unexpected error: %s', e)
         raise
-
+'''
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
